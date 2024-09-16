f7fb1fec5adc093b80b91d1482db09274feee1c5
manishpa04 <manish.pa04@gmail.com>
2024-09-16 13:59:00 +0530

    Add files via upload

---
+++ ILotteryNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryNumbers
+{
+    public interface ILotteryNumber
+    {
+        public List<int> generateSixSetOfNumbers(string minrange, string maxrange);
+    }
+}
---
+++ LotteryNumber.cs
@@ -0,0 +1,31 @@
+﻿namespace LotteryNumbers
+{
+    public class LotteryNumber: ILotteryNumber
+    {
+        private int randomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        public List<int> generateSixSetOfNumbers(string minrange, string maxrange)
+        {
+            int min = 0;
+            int max = 0;
+
+            Int32.TryParse(minrange, out min);
+            Int32.TryParse(maxrange, out max);
+            List<int> lotteryList = new List<int>();
+            int i = 0;
+            while (i < 6)
+            {
+                int number = randomNumber(min, max);
+                if (!lotteryList.Contains(number))
+                {
+                    lotteryList.Add(number);
+                    i++;
+                }
+            }
+            return lotteryList;
+        }
+    }
+}
