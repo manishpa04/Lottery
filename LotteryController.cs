1c0b39f5022e723318309d1f9131eeb41a4c8a99
manishpa04 <manish.pa04@gmail.com>
2024-09-16 13:55:55 +0530

    FirstCommit

---
+++ LotteryController.cs
@@ -0,0 +1,27 @@
+﻿using LotteryNumbers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LotteryGnerator.Controllers
+{
+    [Route("api/Lottery")]
+    [ApiController]
+    public class LotteryController : ControllerBase
+    {
+        private ILotteryNumber lottteryNumber;
+        public LotteryController(ILotteryNumber lottteryNumber)
+        { 
+        this.lottteryNumber = lottteryNumber;
+        }
+        
+        [HttpGet(Name = "getLotteryNumbers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public string getLotteryNumbers( string min,string max)
+        {
+             
+            var result = string.Join<int>(string.Empty, lottteryNumber.generateSixSetOfNumbers(min, max));
+            return result;
+        }
+    }
+}
